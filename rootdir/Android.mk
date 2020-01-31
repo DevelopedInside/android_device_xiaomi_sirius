a8f075c998c0c14f46f7ecbef24e6b48f1f2060b
uixdess <alexeydomoratsky1@gmail.com>
2020-01-31 15:56:51 +0300

    sirius: force push changes from branch lineage-17.1
    * need to make equal two branches for testing new stuff

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE        := fstab.qcom
+LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/fstab.qcom
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)
@@ -9,6 +10,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -16,6 +18,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.leds.sh
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.leds.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
