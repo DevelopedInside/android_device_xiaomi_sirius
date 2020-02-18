996869ba58a979fcc082db0aa5e2dec4d8255493
Bruno Martins <bgcngm@gmail.com>
2020-02-18 23:49:20 +0000

    sirius: Split system and vendor props
    Change-Id: If965be42e68e9ebbd816656dd2af79f541c1ceea

--- device.mk
+++ device.mk
@@ -31,9 +31,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml
 
-# Properties
--include $(DEVICE_PATH)/device-props.mk
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
