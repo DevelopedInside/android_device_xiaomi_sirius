40cb97404f229caaa113e68a8b3c8dd1bf6773d2
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-03-22 11:48:33 -0300

    sirius: Include firmware
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/sirius/sirius-vendor.mk)
 
+# Firmware
+$(call inherit-product, vendor/xiaomi/firmware/sirius/firmware.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2244
 TARGET_SCREEN_WIDTH := 1080
